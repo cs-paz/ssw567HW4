87801a92c6b66a3871d706119717278caeba8f07
Christian Szablewski-Paz <cszablewskipaz@gmail.com>
2021-10-02 21:25:07 -0400

    commit

---
+++ test_GetGithubDetails.py
@@ -0,0 +1,23 @@
+from GetGithubDetails import getGithubDetails
+
+def test_getGithubDetails1():
+  assert getGithubDetails('cs-paz') == [
+    'Repo: ahch_app Number of commits: 30',
+    'Repo: CS-115 Number of commits: 1',
+    'Repo: CS-135 Number of commits: 1',
+    'Repo: CS-146 Number of commits: 5',
+    'Repo: CS-284 Number of commits: 1',
+    'Repo: CS-385 Number of commits: 1',
+    'Repo: CS-392 Number of commits: 1',
+    'Repo: CS-496 Number of commits: 2',
+    'Repo: DuckDorms Number of commits: 7',
+    'Repo: ssw567HW0 Number of commits: 2',
+    'Repo: ssw567HW1 Number of commits: 3',
+    'Repo: ssw567HW2 Number of commits: 8',
+  ]
+
+def test_getGithubDetails2(): 
+  assert getGithubDetails('') == []
+
+def test_getGithubDetails3(): 
+  assert getGithubDetails('this does not exist') == []
